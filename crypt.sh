cc30786e2670450e9d5b5e1f65bee936dbd0928f
Joschka Schwarz <40362019+jwarz@users.noreply.github.com>
2020-05-26 17:44:37 +0200

    crypt locally

--- crypt.sh
+++ crypt.sh
@@ -9,4 +9,4 @@ find ./public/_syllabi/tech_ent/z_cases -type f -name "index.html" -exec staticr
 
 find ./public/_syllabi/gbwl -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_1" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 
-find ./public/_syllabi/dat_sci -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_2" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
+#find ./public/_syllabi/dat_sci -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_2" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
